6aaa0365a913909913645b566e5dbba5aea1cc7e
Valentina Karpova <bfeatb@mail.ru>
2021-07-21 22:58:31 +0100

    feat: implement dynamic cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,43 @@
-let editButton = document.querySelector(".profile__edit-button");
-let editPopup = document.querySelector(".popup");
+const cards = [
+  { 
+    image: "./images/Rectangle.svg",
+    title: "Birthday",
+    alt: "Happy Birthday",
+    url: "#"
+  },
 
-let nameProfile = document.querySelector(".profile__name");
-let descriptionProfile = document.querySelector(".profile__description");
+  { 
+    image: "./images/Rectangle-1.svg",
+    title: "Anniversary",
+    alt: "Good life book",
+    url: "#"
+  },
 
-let popupForm = document.querySelector(".popup__form");
-let nameInput = popupForm.querySelector("input[name='name']");
-let descriptionInput = popupForm.querySelector("input[name='description']");
+  { 
+    image: "./images/Rectangle-2.svg",
+    title: "Valentine's Day",
+    alt: "Kiss me card",
+    url: "#"
+  },
 
-let closeButton = document.querySelector(".popup__close");
+  { 
+    image: "./images/Rectangle-3.svg",
+    title: "Christmas",
+    alt: "Christmas tree",
+    url: "#"
+  },
+];
 
-//Open popup
-function onClickEditButton(){
-  editPopup.classList.add("popup_opened");
-  nameInput.value = nameProfile.textContent;
-  descriptionInput.value = descriptionProfile.textContent;
+const cardTemplate = document.getElementById("cardTemplate");
+const cardsContainer = document.querySelector(".occasions__cards");
+
+for (const card of cards) {
+  const cardNode = cardTemplate.content.cloneNode(true);
+  cardNode.querySelector(".occasions__img").setAttribute("src", card.image);
+  cardNode.querySelector(".occasions__img").setAttribute("alt", card.alt);
+  cardNode.querySelector(".occasions__name").textContent = card.title;
+  cardNode.querySelector(".occasions__link").setAttribute("href", card.url);
+  cardsContainer.appendChild(cardNode);
 }
-
-//Close popup
-function onClickCloseButton(){
-  editPopup.classList.remove("popup_opened");
-}
-
-//Send data from popup to profile.
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  nameProfile.textContent = nameInput.value;
-  descriptionProfile.textContent = descriptionInput.value;
-  onClickCloseButton();
-}
-
-popupForm.addEventListener('submit', formSubmitHandler);
-editButton.addEventListener("click", onClickEditButton);
-closeButton.addEventListener("click", onClickCloseButton);
-
 
 
